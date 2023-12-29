92f5bad8ce050ab7f4aa1bf3eed145c8e16b7906
Aditi Singh <119834735+aditi-singh-21@users.noreply.github.com>
2023-12-29 19:32:56 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -15,37 +15,109 @@
 import streamlit as st
 from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+st.title('Stock Market Prediction')
+
+import pandas as pd
+import numpy as np
+import matplotlib as mpl
+import seaborn as sns
+import matplotlib_inline
+import matplotlib.pyplot as plt 
+import plotly.graph_objects as go
+from mpl_finance import candlestick_ochl
+from mpl_finance import candlestick_ohlc
+import plotly_express as px
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense, LSTM
+import yfinance as yf
+from pandas_datareader import DataReader
+from pandas_datareader import data as pdr
+from datetime import datetime
+import pandas_datareader as data
+
+startdate=datetime(2013,4,1)
+enddate=datetime.now()
+yf.pdr_override()
+user_input=st.text_input('Enter Stock Ticker','AAPL')
+df=pdr.get_data_yahoo([user_input],start=startdate,end=enddate)
+
+st.subheader('Data from 2013 to present')
+st.write(df.describe())
+
+st.subheader('Closing price VS Time')
+fig=plt.figure(figsize=(12,6))
+plt.plot(df.Close)
+st.pyplot(fig)
+
+st.subheader('Closing price VS Time with 100MA')
+ma100=df.Close.rolling(100).mean()
+fig=plt.figure(figsize=(12,6))
+plt.plot(ma100)
+plt.plot(df.Close)
+plt.legend(["100 MA","Close"])
+st.pyplot(fig)
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+st.subheader('Closing price VS Time with 100MA and 200MA')
+ma100=df.Close.rolling(100).mean()
+ma200=df.Close.rolling(200).mean()
+fig=plt.figure(figsize=(12,6))
+plt.plot(ma100)
+plt.plot(ma200)
+plt.plot(df.Close)
+plt.legend(["100 MA","200 MA","Close"])
+st.pyplot(fig)
+print(df.shape)
 
-    st.write("# Welcome to Streamlit! 👋")
+data = df.filter(['Close'])
+dataset = data.values
+training_data_len = int(np.ceil( len(dataset) * .95 ))
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(dataset)
+train_data = scaled_data[0:int(training_data_len), :]
+x_train = []
+y_train = []
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+    if i<= 61:
+        print(x_train)
+        print(y_train)
+        print()
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+model = Sequential()
+model.add(LSTM(128, return_sequences=True, input_shape= (x_train.shape[1], 1)))
+model.add(LSTM(64, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+test_data = scaled_data[training_data_len - 60: , :]
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1 ))
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+train = data[:training_data_len]
+valid = data[training_data_len:]
+valid['Predictions'] = predictions
+fig2=plt.figure(figsize=(16,6))
+st.subheader('Original vs Predicsted')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+st.pyplot(fig2)
 
 
-if __name__ == "__main__":
-    run()
+st.write(valid)
